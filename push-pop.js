631d674dcf139573e75171c46c331df770bb3a69
Ishtiaque1997 <ishtiaque07031997@gmail.com>
2023-06-07 13:05:11 +0600

    more loaded now

---
+++ push-pop.js
@@ -0,0 +1,14 @@
+var arr=[23,45,67,89,90];
+//unshift adds at beginning.
+arr.unshift(87);
+console.log(arr);
+//shift removes from beginning
+arr.shift();
+console.log(arr);
+
+//push adds an element in the last of an array.
+arr.push(9000);
+// console.log(arr);
+var element=arr.pop();
+//pops removes an element from the last of an array.
+// console.log(element);
