121677736c60052969c7bb8a3ae5d3450b40746f
Ishtiaque1997 <ishtiaque07031997@gmail.com>
2023-06-07 16:28:59 +0600

    more loaded noww

---
+++ comparison.js
@@ -0,0 +1,24 @@
+ console.log(4!=8);
+ console.log(4==4);
+ console.log(3 >= 3);
+
+var num1=78;
+var num2=90;
+console.log(num1==num2);
+console.log(num1!==num2);
+console.log(num1>=num2);
+console.log(num1<=num2);
+
+
+//multiple conditions
+// &&(and) ||(or)
+
+var num3=23;
+var num4=56;
+
+if((num3>num4) || (num3=23)){
+ console.log(90);
+}
+
+
+
